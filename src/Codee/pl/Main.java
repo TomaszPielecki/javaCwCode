ffdd6eb93224c6ddab9cdf5d4f983a75c163bd22
Tomasz Pielecki <tomasz.pielecki86@gmail.com>
2021-10-10 22:37:28 +0200

    Initial commit

---
+++ src/Codee/pl/Main.java
@@ -0,0 +1,14 @@
+package Codee.pl;
+import java.lang.*;
+public class Main {
+
+    public static void main(String[] args) {
+	String country="Spain";
+	country="Germany";
+	country="Finland";
+	int number=22;
+	number=55;
+	Boolean val1=true;
+        System.out.println(country+" "+number+" "+val1);
+    }
+}
